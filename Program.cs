fd9b369d7070ed866508c4dd0ac184a8a87d80ef
Yuri <antufev.1996@inbox.ru>
2021-03-30 20:28:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пожалуйста имя");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня  {DateTime.Now} ");
+            Console.ReadKey();
+        }
+    }
+}
